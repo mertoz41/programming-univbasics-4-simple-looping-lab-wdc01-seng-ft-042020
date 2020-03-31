e059e44e89f6a539150916268356787a1bcf3d76
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-31 01:40:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,18 @@
 # Write your methods here
+def loop_message_five_times(message)
+  i = 0 
+  while i < 5  do 
+    puts "message"
+  end 
+  i++
+end 
+
+
+def loop_message_n_times
+end 
+
+def output_array
+end 
+
+def return_string_array 
+end 
